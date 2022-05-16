96a0054961af36e87f000f4b50326f58022e21ec
jcvinz <jonathancalvin21036@gmail.com>
2022-05-16 14:35:00 +0700

    initial commit

---
+++ app/src/main/java/com/kitabisa/scholarshipmanagement/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kitabisa.scholarshipmanagement.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kitabisa.scholarshipmanagement.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
