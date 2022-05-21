ba4c6bcaa53a153f45b788a9a0b0cb40d15afb50
drammehjunior <mameddram@gmail.com>
2022-05-21 17:22:50 +0700

    added login logic codes

--- app/src/main/java/com/kitabisa/scholarshipmanagement/ui/MainActivity.kt
+++ app/src/main/java/com/kitabisa/scholarshipmanagement/ui/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.kitabisa.scholarshipmanagement.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GetTokenResult
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.options
 import com.kitabisa.scholarshipmanagement.R
+import com.kitabisa.scholarshipmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun signOut() {
+        GoogleSignIn.getClient(this, GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()).signOut()
+        auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
--- app/src/main/java/com/kitabisa/scholarshipmanagement/ui/RegisterActivity.kt
+++ app/src/main/java/com/kitabisa/scholarshipmanagement/ui/RegisterActivity.kt
@@ -5,14 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import com.kitabisa.scholarshipmanagement.R
+import com.kitabisa.scholarshipmanagement.databinding.ActivityMainBinding
+import com.kitabisa.scholarshipmanagement.databinding.ActivityRegisterBinding
+import com.kitabisa.scholarshipmanagement.utils.isValidEmail
 
 class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupView()
+
+        binding.btnSubmit.setOnClickListener {
+            val noErrorResult =  inputFieldFilled()
+            if(noErrorResult){
+                Toast.makeText(this, "All data is good", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Some data is invalid", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 
 
@@ -27,5 +44,53 @@ class RegisterActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+
+
+
+
+    private fun inputFieldFilled(): Boolean{
+        var noError = true
+        val emailInput = binding.emailInput.editText
+        val firstnameInput = binding.firstNameInput
+        val lastnameInput = binding.lastNameInput
+        val passwordInput = binding.passwordInput
+
+
+        //cek email
+        if(!isValidEmail(emailInput?.text.toString()))
+        {
+            binding.emailInput.error = "invalid email"
+            noError = false
+        }else{
+            binding.emailInput.isErrorEnabled = false
+        }
+
+        //cek nama depan
+        if(firstnameInput.editText?.text?.isEmpty() == true){
+            firstnameInput.error = "First name cannot be empty"
+            noError = false
+        }else{
+            firstnameInput.isErrorEnabled = false
+        }
+
+        //cek nama belakang
+        if(lastnameInput.editText?.text?.isEmpty() == true){
+            lastnameInput.error = "Last name cannot be empty"
+            noError = false
+        }else{
+            lastnameInput.isErrorEnabled = false
+        }
+
+        //cek password
+        if(passwordInput.editText?.text?.isEmpty() == true){
+            passwordInput.error = "Password cannot be empty"
+            noError = false
+        }else{
+            passwordInput.isErrorEnabled = false
+        }
+
+        return noError
     }
 }
