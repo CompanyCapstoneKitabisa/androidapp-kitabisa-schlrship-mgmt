9138e831ef774f96587a66f44881c342c8bdc7f9
jcvinz <jonathancalvin21036@gmail.com>
2022-05-25 22:48:22 +0700

    Added Image on Splash Screen

--- app/src/main/java/com/kitabisa/scholarshipmanagement/ui/MainActivity.kt
+++ app/src/main/java/com/kitabisa/scholarshipmanagement/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.kitabisa.scholarshipmanagement.ui
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.kitabisa.scholarshipmanagement.databinding.ActivityMainBinding
 import com.kitabisa.scholarshipmanagement.ui.login.LoginActivity
 
@@ -14,7 +17,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
         val intent = Intent(this, LoginActivity::class.java)
 
         Handler(mainLooper).postDelayed({
