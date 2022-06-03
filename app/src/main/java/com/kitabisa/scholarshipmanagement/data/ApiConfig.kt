e46ebb0d6639a59d066f2da5c41c79a7596e5271
nyomanjyotisa <jyotisa1616@gmail.com>
2022-06-03 17:31:54 +0800

    empty state application list

--- app/src/main/java/com/kitabisa/scholarshipmanagement/data/ApiConfig.kt
+++ app/src/main/java/com/kitabisa/scholarshipmanagement/data/ApiConfig.kt
@@ -5,6 +5,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiConfig {
     fun getApiService(): ApiService {
@@ -14,6 +15,9 @@ object ApiConfig {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
         val client: OkHttpClient = OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.MINUTES)
+            .readTimeout(5, TimeUnit.MINUTES)
+            .writeTimeout(5, TimeUnit.MINUTES)
             .addInterceptor(loggingInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
